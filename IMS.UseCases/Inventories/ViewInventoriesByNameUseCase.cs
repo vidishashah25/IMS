002e4e1d194fa1763328c6772426c4638aef98f0
Vidisha Shah <vidisha.shah14@gmail.com>
2025-08-06 18:00:16 +0200

    Inject the Use Case in the Razor Component

--- IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.PluginInterfaces;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace IMS.UseCases.Inventories
 {
-    public class ViewInventoriesByNameUseCase
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
     {
         private readonly IInventoryRepository inventoryRepository;
 
@@ -16,9 +17,9 @@ namespace IMS.UseCases.Inventories
         {
             this.inventoryRepository = inventoryRepository;
         }
-        public async Task<IEnumerable<Inventory>> ExecuteAsync (string name= "")
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
         {
-           return await inventoryRepository.GetInventoriesByNameAsync(name);
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
         }
     }
 }
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -1,4 +1,6 @@
 using IMS.Plugins.InMemory;
+using IMS.UseCases.Inventories;
+using IMS.UseCases.Inventories.Interfaces;
 using IMS.UseCases.PluginInterfaces;
 using IMS.WebApp.Components;
 
@@ -6,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
+
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
+
+builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 
 var app = builder.Build();
 
