24e2384880e5fe0bc36f379aca08cf9588c54378
Vidisha Shah <vidisha.shah14@gmail.com>
2025-08-06 15:31:00 +0200

    Create a plugin with dependency injection

--- IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace IMS.UseCases.Inventories
 {
-    internal class ViewInventoriesByNameUseCase
+    public class ViewInventoriesByNameUseCase
     {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync (string name= "")
+        {
+           return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
     }
 }
