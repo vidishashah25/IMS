9b15aff14c91c5a10f87db6361bb686a8e0b4af9
Vidisha Shah <vidisha.shah14@gmail.com>
2025-08-07 19:53:47 +0200

    Add inventory feaure successfully added with Validation

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -18,6 +18,20 @@ namespace IMS.Plugins.InMemory
             };
         }
 
+        public Task AddInventoryAsync(Inventory inventory)
+        {
+            if(_inventories.Any(x => x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
+            {
+                return Task.CompletedTask;
+            }
+
+            var maxId = _inventories.Max(x => x.InventoryId);
+            inventory.InventoryId = maxId+1;
+
+            _inventories.Add(inventory);
+            return Task.CompletedTask;
+        }
+
         public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
         {
             if (string.IsNullOrWhiteSpace(name)) return await Task.FromResult(_inventories);
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 
+builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
