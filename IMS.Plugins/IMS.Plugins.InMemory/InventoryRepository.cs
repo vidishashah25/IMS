12223fcb175dc2d4fa2721c16dcad5674d4efc03
vidishashah25 <vidisha.shah14@gmail.com>
2025-10-14 11:23:20 +0200

    Implemented AddInventory UseCase

--- IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -19,11 +19,25 @@ namespace IMS.Plugins.InMemory
             };
         }
 
+        public Task AddInventoryAsync(Inventory inventory)
+        {
+            if(_inventories.Any(x => x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
+            { return Task.CompletedTask; }
+            
+            var maxId = _inventories.Max(x => x.InventoryId);
+            inventory.InventoryId = maxId;
+
+            _inventories.Add(inventory);
+            return Task.CompletedTask;
+        }
+
         public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
         {
             if(string.IsNullOrWhiteSpace(name)) return await Task.FromResult(_inventories);
 
             return _inventories.Where(x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
         }
+
+
     }
 }
--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -9,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents();
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
+
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
+builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
 
 var app = builder.Build();
 
