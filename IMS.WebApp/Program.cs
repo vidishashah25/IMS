76c40ac2a8cd36f13d83b30cc918aa9f1db7557c
vidishashah25 <vidisha.shah14@gmail.com>
2025-10-15 10:20:40 +0200

    Implemented Edit Inventory Page Component

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
 builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
 builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
+builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
 
 var app = builder.Build();
 
