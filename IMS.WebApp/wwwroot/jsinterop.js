25c6fdcd4a956753d7641ed7f597601f0481dacb
vidishashah25 <vidisha.shah14@gmail.com>
2025-10-21 15:41:57 +0200

    Js InterOp

---
+++ IMS.WebApp/wwwroot/jsinterop.js
@@ -0,0 +1,13 @@
+﻿
+function preventFormSubmission(formId) {
+
+    document.getElementById(`${formId}`).addEventListener('keydown', function (event) {
+
+        if (event.key == "Enter") {
+            event.preventDefault();
+            return false;
+        }
+
+    });
+
+}
