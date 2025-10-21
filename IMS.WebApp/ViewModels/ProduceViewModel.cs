41a21eef7f4d22cfde029319b4c823ef299b1e3c
vidishashah25 <vidisha.shah14@gmail.com>
2025-10-21 14:37:22 +0200

    Produce Product Page Component

---
+++ IMS.WebApp/ViewModels/ProduceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using IMS.CoreBusiness;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class ProduceViewModel
+    {
+        [Required]
+        public string ProductionNumber { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "You have to select a product.")]
+        public int ProductId { get; set; }
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater or equal to 1.")]
+        public int QuantityToProduce { get; set; }
+
+        public Product? Product { get; set; }
+    }
+}
